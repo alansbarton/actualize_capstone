a89dbba867c21007e37582ed55ef395f6003e293
Alan Barton <abarton201@gmail.com>
2020-10-15 22:20:15 -0400

    add rought user index and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+  end
 end
